857394e6d46b2bee5001fa24052c3d1de27b4234
thiery <thiery@marmelab.com>
2019-11-27 15:12:01 +0100

    rotate labyrinth based on phone orientation

--- src/Labyrinth3D.js
+++ src/Labyrinth3D.js
@@ -27,9 +27,10 @@ export default ({ width, height, xAcceleration, yAcceleration }) => {
             {safe && <button onClick={go}>GO</button>}
             <x3d is="x3d" width={`${width}px`} height={`${height}px`} onClick={go}>
                 <scene onClick={go} is="scene">
+                    <navigationInfo is="navigationInfo" type='"none"' id="navType"></navigationInfo>
                     <viewpoint
                         is="viewpoint"
-                        bind="true"
+                        bind="TRUE"
                         centerOfRotation={`${width / 200},${-height / 200},0`}
                         isActive
                         position={`${width / 200},${-height / 200},13`}
@@ -51,96 +52,108 @@ export default ({ width, height, xAcceleration, yAcceleration }) => {
                         shadowsplitoffset="0.1"
                     ></directionallight>
                     <transform is="transform" translation="0,0,0">
-                        <group is="group">
-                            <Sphere
-                                x={x / 100 + 0.2}
-                                y={-y / 100 - 0.2}
-                                z={0.2}
-                                radius={0.2}
-                                texture={metal}
-                            />
-                            <group is="group">
-                                {maze.map(row =>
-                                    row.map(({ x, y, top, left, bottom, right }) => {
-                                        return (
-                                            <>
-                                                {top && (
-                                                    <Box
-                                                        x={x + 0.5}
-                                                        y={-y}
-                                                        width={1}
-                                                        height={0.1}
-                                                        depth={1}
-                                                        texture={wood}
-                                                    />
-                                                )}
-                                                {bottom && (
-                                                    <Box
-                                                        x={x + 0.5}
-                                                        y={-y - 1}
-                                                        width={1}
-                                                        height={0.1}
-                                                        depth={1}
-                                                        texture={wood}
-                                                    />
-                                                )}
-                                                {left && (
-                                                    <Box
-                                                        x={x}
-                                                        y={-y - 0.5}
-                                                        width={0.1}
-                                                        height={1}
-                                                        depth={1}
-                                                        texture={wood}
-                                                    />
-                                                )}
-                                                {right && (
-                                                    <Box
-                                                        x={x + 1}
-                                                        y={-y - 0.5}
-                                                        width={0.1}
-                                                        height={1}
-                                                        depth={1}
-                                                        texture={wood}
-                                                    />
-                                                )}
-                                            </>
-                                        );
-                                    }),
-                                )}
-                            </group>
-                            <Plane
-                                x={width / 200}
-                                y={-height / 200}
-                                width={width / 100}
-                                height={height / 100}
-                                texture={wood}
-                            />
-                            <group is="group">
-                                {holes.map(({ x, y }) => {
-                                    return (
+                        <transform
+                            is="transform"
+                            rotation={`0 1 0 ${xAcceleration / 100}`}
+                            center={`${width / 200},${-height / 200},0`}
+                        >
+                            <transform
+                                is="transform"
+                                rotation={`1 0 0 ${yAcceleration / 100}`}
+                                center={`${width / 200},${-height / 200},0`}
+                            >
+                                <group is="group">
+                                    <Sphere
+                                        x={x / 100 + 0.2}
+                                        y={-y / 100 - 0.2}
+                                        z={0.2}
+                                        radius={0.2}
+                                        texture={metal}
+                                    />
+                                    <group is="group">
+                                        {maze.map(row =>
+                                            row.map(({ x, y, top, left, bottom, right }) => {
+                                                return (
+                                                    <>
+                                                        {top && (
+                                                            <Box
+                                                                x={x + 0.5}
+                                                                y={-y}
+                                                                width={1}
+                                                                height={0.1}
+                                                                depth={1}
+                                                                texture={wood}
+                                                            />
+                                                        )}
+                                                        {bottom && (
+                                                            <Box
+                                                                x={x + 0.5}
+                                                                y={-y - 1}
+                                                                width={1}
+                                                                height={0.1}
+                                                                depth={1}
+                                                                texture={wood}
+                                                            />
+                                                        )}
+                                                        {left && (
+                                                            <Box
+                                                                x={x}
+                                                                y={-y - 0.5}
+                                                                width={0.1}
+                                                                height={1}
+                                                                depth={1}
+                                                                texture={wood}
+                                                            />
+                                                        )}
+                                                        {right && (
+                                                            <Box
+                                                                x={x + 1}
+                                                                y={-y - 0.5}
+                                                                width={0.1}
+                                                                height={1}
+                                                                depth={1}
+                                                                texture={wood}
+                                                            />
+                                                        )}
+                                                    </>
+                                                );
+                                            }),
+                                        )}
+                                    </group>
+                                    <Plane
+                                        x={width / 200}
+                                        y={-height / 200}
+                                        width={width / 100}
+                                        height={height / 100}
+                                        texture={wood}
+                                    />
+                                    <group is="group">
+                                        {holes.map(({ x, y }) => {
+                                            return (
+                                                <Cylinder
+                                                    key={`${x}-${y}`}
+                                                    x={x + 0.5}
+                                                    y={-y - 0.5}
+                                                    rotation={`1,0,0,${Math.PI / 2}`}
+                                                    radius={0.2}
+                                                    height={0.1}
+                                                    diffuseColor="0 0 0"
+                                                />
+                                            );
+                                        })}
+
                                         <Cylinder
-                                            key={`${x}-${y}`}
-                                            x={x + 0.5}
-                                            y={-y - 0.5}
+                                            x={goal.x + 0.5}
+                                            y={-goal.y - 0.5}
                                             rotation={`1,0,0,${Math.PI / 2}`}
                                             radius={0.2}
                                             height={0.1}
-                                            diffuseColor="0 0 0"
+                                            diffuseColor="0 1 0"
                                         />
-                                    );
-                                })}
-
-                                <Cylinder
-                                    x={goal.x + 0.5}
-                                    y={-goal.y - 0.5}
-                                    rotation={`1,0,0,${Math.PI / 2}`}
-                                    radius={0.2}
-                                    height={0.1}
-                                    diffuseColor="0 1 0"
-                                />
-                            </group>
-                        </group>
+                                    </group>
+                                </group>
+                            </transform>
+                        </transform>
                     </transform>
                 </scene>
             </x3d>
