d9b58bfa2c7347854b93b5e99db5ea0923e06093
thiery <thiery@marmelab.com>
2021-02-03 11:53:54 +0100

    bootstrap react-three-fiber

---
+++ src/LabyrinthReactThreeFiber.js
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+
+const Box = props => {
+    // This reference will give us direct access to the mesh
+    const mesh = useRef();
+
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false);
+    const [active, setActive] = useState(false);
+
+    // Rotate mesh every frame, this is outside of React without overhead
+    useFrame(() => {
+        mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    });
+
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+            onClick={event => setActive(!active)}
+            onPointerOver={event => setHover(true)}
+            onPointerOut={event => setHover(false)}
+        >
+            <boxBufferGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    );
+};
+
+const Labyrinth = () => {
+    return (
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+        </Canvas>
+    );
+};
+
+export default Labyrinth;
